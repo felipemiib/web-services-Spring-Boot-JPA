12afe1ab1469bc4fb43515c0f08fd1752516381c
felipemiib <felipemiib@gmail.com>
2021-11-23 00:12:04 -0300

    Service layer, component registration

--- course/src/main/java/com/mahalo/course/resources/UserResource.java
+++ course/src/main/java/com/mahalo/course/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.mahalo.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mahalo.course.entities.User;
+import com.mahalo.course.services.UserService;
 
 @RestController // informa que é gerenciado por um controlador
-@RequestMapping(value = "/users") //indica qual dado o controlador vai seguir
+@RequestMapping(value = "/users") //indica qual dado o controlador vai seguir || API localhost:8080/users
 public class UserResource {
 
-	@GetMapping //coletar ID : http://localhost:8080/users
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	@Autowired
+	private UserService service;
+	
+	@GetMapping //coletar ID : http://localhost:8080/users || FIND ALL
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);	
+	}
+
 }
